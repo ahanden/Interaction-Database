2721685af90a566d763ec3d04901dc07be91daaf
Adam Handen <adam.handen@gmail.com>
2016-05-19 14:13:44 -0400

    Fixed ANOTHER glitch in the SignalLink file

--- signallink_correct.sh
+++ signallink_correct.sh
@@ -6,6 +6,9 @@
 
 fname=$1
 
+# They forgot a bar dilimiter for some of the entries
+sed -i 's/\Sentrez gene/|entrez gene/g' $fname
+
 # They mislabeled Ensembl genes as Entrez genes
 sed -i 's!entrez gene/locuslink:ENSG!ensembl:ENSG!g' $fname
 
