49482ba277517aac5cf7f21a0db65c6d9dcd21ea
Adam Handen <adam.handen@gmail.com>
2016-05-19 10:23:01 -0400

    Added script to correct SignalLink screwups (Seriously, they cannot properly identify anything!)

---
+++ signallink_correct.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# So the SignalLink people have *no* idea how to format a MI-TAB file,
+# and screw up most of the identifier labels. This script is meant to
+# correct those labels.
+
+fname=$1
+
+# They mislabeled Ensembl genes as Entrez genes
+sed -i 's!entrez gene/locuslink:ENSG!ensembl:ENSG!g' $fname
+
+# They also mislabled mirBase genes as uniprot
+sed -i 's/uniprotkb:\(MI[0-9]\{7\}\)/mirbase:\1/g' $fname
